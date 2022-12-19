6651bba86f51d5225dfe9165c7f6abf69fbca126
MyUsername <alberto.baglivi@gmail.com>
2022-12-19 20:45:26 +0100

    Add the table on layout

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -1,14 +1,55 @@
 @extends('layouts.app')
 
-@section('title', 'All houses')
+@section('title', 'All train')
 
 @section('content')
     <div class="container">
         <h2>La lista dei treni</h2>
-        <ul>
-            @foreach ($trains as $train)
-                <li>{{ $train->Azienda }}, {{ $train->Codice_Treno }}</li>
-            @endforeach
-        </ul>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Azienda</th>
+                    <th scope="col">Stazione di partenza</th>
+                    <th scope="col">Stazione di arrivo</th>
+                    <th scope="col">Orario di partenza</th>
+                    <th scope="col">Orario di arrivo</th>
+                    <th scope="col">Codice Treno</th>
+                    <th scope="col">Numero Carrozze</th>
+                    <th scope="col">In orario</th>
+                    <th scope="col">Cancellato</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                    <tr>
+                        <th scope="row">{{ $train->Azienda }}</th>
+                        <td>{{ $train->Stazione_di_partenza }}</td>
+                        <td>{{ $train->Stazione_di_arrivo }}</td>
+                        <td>{{ $train->Orario_di_partenza }}</td>
+                        <td>{{ $train->Orario_di_arrivo }}</td>
+                        <td>{{ $train->Codice_Treno }}</td>
+                        <td>{{ $train->Numero_Carrozze }}</td>
+                        <td>{{ $train->In_orario }}</td>
+                        <td>{{ $train->Cancellato }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
     </div>
 @endsection
+
+
+
+
+
+
+
+
+{{-- 
+
+<ul>
+    @foreach ($trains as $train)
+        <li>{{ $train->Azienda }}, {{ $train->Codice_Treno }}</li>
+    @endforeach
+</ul> --}}

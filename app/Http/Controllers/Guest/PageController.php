7bbbdf04ab83ee2c21ebead4ce943eaa464f0713
MyUsername <alberto.baglivi@gmail.com>
2022-12-20 10:47:35 +0100

    Add the where on Controller

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use App\Models\Train; 
 use Illuminate\Http\Request;
+use Nette\Utils\DateTime;
 
 class PageController extends Controller
 {
     public function index() {
-        $trains = Train::all();
+
+        $trains = Train::where('Data_partenza', '>=' , now())->get();
         return view('trains.index', compact('trains'));
     }
 }
